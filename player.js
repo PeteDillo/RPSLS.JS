94f2828c8da4352060a363e9de63a6a5d5943fc6
William Hagan <williamhagan1728@yahoo.com>
2022-03-15 19:18:25 -0700

    Start player class and constructor

--- player.js
+++ player.js
@@ -5,9 +5,42 @@ constructor()
 }
 
 class Human{
-// we pick paper
+    constructor(name) {
+        this.name = name;
+        this.count = 0;
+        this.gestures = {
+            
+            0: {
+                name: 'rock',
+                weakness: ['paper', 'spock']
+            },
+            
+            1: {
+                name: 'paper',
+                weakness: ['scissors', 'lizard']
+            },
+            
+            2: {
+                name: 'scissors',
+                weakness: ['rock', 'spock']
+            },
+            
+            3: {
+                name: 'lizard',
+                weakness: ['rock', 'scissors']
+            },
+            
+            4: {
+                name: 'spock',
+                weakness: ['lizard', 'paper']
+            },
+        }
+        this.hand = this.gestures.rock;
+    }
+
 }
 
 class AI{
+    constructor()
 
 }
