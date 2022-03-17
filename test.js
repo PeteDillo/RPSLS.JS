394ee56d7fea0be7db5b519a5ef1fdc0c9882219
William Hagan <williamhagan1728@yahoo.com>
2022-03-17 16:45:06 -0700

    Add logic to test.js

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 const Gesture = require("./gesture");
 let Rock = new Gesture('Rock', 'Crush', ['Cut','Smash'] );
 console.log({Rock})
+
+let Paper = new Gesture('Paper' , 'Covers' , ['Eats' , 'Cut'])
+console.log({Paper})
+
+let Scissors = new Gesture('Scissors' , 'Cut' , ['Smash' , 'Crush'])
+console.log({Scissors})
+
+let Spock = new Gesture('Spock' , 'Smash' , ['Poison' , 'Disprove'])
+console.log({Spock})
+
+let Lizard = new Gesture('Lizard' , 'Eat' ['Decapitate' , 'Crush'])
+console.log({Lizard})
+
